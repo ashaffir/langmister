f8b68356620843fe9757bc52834abd20e9bd34e9
ashaffir <alfred.shaffir@gmail.com>
2023-04-12 17:48:48 +0300

    cors

--- transmister/transmister/settings.py
+++ transmister/transmister/settings.py
@@ -34,6 +34,10 @@ DEBUG = bool(os.getenv("DEBUG"))
 
 ALLOWED_HOSTS = ["*"]
 
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_WHITELIST = [
+#     'localhost',
+# ]
 
 # Application definition
 
@@ -47,11 +51,13 @@ INSTALLED_APPS = [
     # My apps
     "main",
     "django_social_share",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
